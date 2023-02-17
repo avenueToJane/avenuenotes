1cd61df222f64e39f2a62f5ae135664765e7eb73
大道至简 <289536143@qq.com>
2023-02-17 20:15:45 +0800

    2023/02/17 周五

--- commit.bat
+++ commit.bat
@@ -6,17 +6,17 @@ set /a timemin=30
 
 set /a timeval=%timemin%
 :loop
-echo LOOP_BEGIN[%time%]:%timeval%====== >> logfile.log
+echo LOOP_BEGIN[%time%]:%timeval%====== >> E://Gitbook//logfile.log
 echo exe_running >> logfile.log
 set /a timeval=%timemin%
 git add *
 (git commit -m "%date%") | find /i "nothing to commit"
 if ERRORLEVEL 1 (
-     echo "there are changes, push it to origin" >> logfile.log
+     echo "there are changes, push it to origin" >>E://Gitbook//logfile.log
        git push -u origin master
 )^
 else (
-     echo "nothing to commit" >> logfile.log
+     echo "nothing to commit" >> E://Gitbook//logfile.log
 )
 timeout /nobreak /t %timeval%
 goto loop
